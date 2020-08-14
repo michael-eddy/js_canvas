0a07ae7d18f811c52192df75ee8585b43aad4d32
unknown <michael19950415@outlook.com>
2020-08-14 09:44:00 +0800

    modified:   jquery.scrollMe.js

--- jquery.scrollMe/jquery.scrollMe.js
+++ jquery.scrollMe/jquery.scrollMe.js
@@ -90,37 +90,73 @@ var scrollMe = function () {
         }
         return item;
     }
+    /*
+        调用该方法使元素从上到下滚动
+    */
     function topToBottom(element, speed) {
-        setStyle(element);
+        setClass(element);
         var dh = $(element).parent().height();
         var interval = setInterval(function () {
-            var p = dh;
-            if ($(element).attr('p') != undefined && $(element).attr('p') != null)
-                p = parseInt($(element).attr('p'));
-            p -= 1;
-            if (p <= -$(element).height())
-                p = dh;
-            $(element).attr('p', p);
-            $(element).attr('style', 'top:' + p + 'px');
+            var top = getTop(element);
+            if (top == undefined)
+                top = dh;
+            top -= 1;
+            if (top <= -$(element).height())
+                top = dh;
+            setStyle(element, top);
         }, speed);
         return interval;
     }
+    /*
+        调用该方法使元素从下到上滚动
+    */
     function bottomToTop(element, speed) {
-        setStyle(element);
+        setClass(element);
         var dh = $(element).parent().height();
         var interval = setInterval(function () {
-            var p = 0;
-            if ($(element).attr('p') != undefined && $(element).attr('p') != null)
-                p = parseInt($(element).attr('p'));
-            p += 1;
-            if (p >= dh)
-                p = 0;
-            $(element).attr('p', p);
-            $(element).attr('style', 'top:' + p + 'px');
+            var top = getTop(element);
+            if (top == undefined)
+                top = 0;
+            top += 1;
+            if (top >= dh)
+                top = 0;
+            setStyle(element, top);
         }, speed);
         return interval;
     }
-    function setStyle(element) {
+    /*
+        调用该方法获取style中的top值
+    */
+    function getTop(element) {
+        var style = $(element).attr('style'), top = undefined;
+        if (style != undefined && style != null && style != '') {
+            var array = style.split(';');
+            for (var index = 0; index < array.length; index++) {
+                if (array[index].indexOf('top') > -1) {
+                    top = parseFloat(array[index].split(':')[1]);
+                    break;
+                }
+            }
+        }
+        return top;
+    }
+    /*
+        调用该方法重组style值
+    */
+    function setStyle(element, top) {
+        var style = $(element).attr('style');
+        var output = '';
+        if (style != undefined && style != null && style != '') {
+            var array = style.split(';');
+            array.forEach(sl => {
+                if (sl.indexOf('top') == -1)
+                    output += sl;
+            });
+        }
+        output += "top:" + top + "px";
+        $(element).attr('style', output);
+    }
+    function setClass(element) {
         if (!$(element).hasClass('scrollMe'))
             $(element).addClass('scrollMe');
     }
